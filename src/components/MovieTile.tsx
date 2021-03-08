671b7570fc387f478bcc65dea8cd92069ab65044
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-08 18:14:12 +0100

    Add movie ids and media types to be able to identify items

--- src/components/MovieTile.tsx
+++ src/components/MovieTile.tsx
@@ -6,15 +6,22 @@ export interface MovieTileProps {
   title: string;
   score: string;
   overview: string;
+  id: string;
+  media_type: 'tv' | 'movie';
 }
 
 const MovieTile: React.FC<MovieTileProps> = props => (
-  <div className="Item" style={{ backgroundImage: 'url(' + props.backdrop + ')' }}>
+  <div
+    className="Item"
+    id={props.id}
+    data-mediatype={props.media_type}
+    style={{ backgroundImage: 'url(' + props.backdrop + ')' }}
+  >
     <div className="overlay">
       <div className="title">{props.title}</div>
       <div className="rating">{props.score} / 10</div>
       <div className="plot">{props.overview}</div>
-      <ExtensionSlot name="ListToggle"></ExtensionSlot>
+      <ExtensionSlot name="ListToggle" params={props}></ExtensionSlot>
     </div>
   </div>
 );
