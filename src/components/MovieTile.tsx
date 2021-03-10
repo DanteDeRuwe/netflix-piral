da5d17bbd827cad59b349b67cf59d87975daca06
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-10 11:02:32 +0100

    Add placeholder backdrop if none defined

--- src/components/MovieTile.tsx
+++ src/components/MovieTile.tsx
@@ -10,20 +10,24 @@ export interface MovieTileProps {
   media_type: 'tv' | 'movie';
 }
 
-const MovieTile: React.FC<MovieTileProps> = props => (
-  <div
-    className="Item"
-    id={props.id}
-    data-mediatype={props.media_type}
-    style={{ backgroundImage: 'url(' + props.backdrop + ')' }}
-  >
-    <div className="overlay">
-      <div className="title">{props.title}</div>
-      <div className="rating">{props.score} / 10</div>
-      <div className="plot">{props.overview}</div>
-      <ExtensionSlot name="ListToggle" params={props}></ExtensionSlot>
+const MovieTile: React.FC<MovieTileProps> = props => {
+  const backDrop = props.backdrop.match(/.*(null|undefined)$/) ? 'https://i.imgur.com/QVaMho2.png' : props.backdrop;
+
+  return (
+    <div
+      className="Item"
+      id={props.id}
+      data-mediatype={props.media_type}
+      style={{ backgroundImage: `url(${backDrop})` }}
+    >
+      <div className="overlay">
+        <div className="title">{props.title}</div>
+        <div className="rating">{props.score} / 10</div>
+        <div className="plot">{props.overview}</div>
+        <ExtensionSlot name="ListToggle" params={props}></ExtensionSlot>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MovieTile;
