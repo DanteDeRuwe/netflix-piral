865a3e9227149f5894a7b8b6e85211772d08e9bb
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-11 11:41:46 +0100

    Add footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,22 +2,28 @@ import * as React from 'react';
 import { ExtensionSlot, LayoutProps, Menu } from 'piral';
 import Logo from './Logo';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Footer from './Footer';
 
 const App: React.FC<LayoutProps> = ({ children }) => (
-  <Router>
-    <header className="Header">
-      <Logo />
-      <Menu type="general" />
-      <ExtensionSlot name="search"></ExtensionSlot>
-      <div className="header-right">
-        <ExtensionSlot name="header-right"></ExtensionSlot>
-      </div>
-    </header>
-    <Route exact path="/">
-      <Redirect to="/browse"></Redirect>
-    </Route>
-    {children}
-  </Router>
+  <>
+    <div className="main-wrapper">
+      <Router>
+        <header className="Header">
+          <Logo />
+          <Menu type="general" />
+          <ExtensionSlot name="search"></ExtensionSlot>
+          <div className="header-right">
+            <ExtensionSlot name="header-right"></ExtensionSlot>
+          </div>
+        </header>
+        <Route exact path="/">
+          <Redirect to="/browse"></Redirect>
+        </Route>
+        {children}
+      </Router>
+    </div>
+    <Footer />
+  </>
 );
 
 export default App;
